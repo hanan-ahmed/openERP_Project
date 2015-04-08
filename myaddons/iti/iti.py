e2d810689714f1217804eb402e79b49e679a2307
zeinabroshdy <zeinabroshdy2014@gmail.com>
2015-04-08 09:20:06 +0200

    this is version2

--- myaddons/iti/iti.py
+++ myaddons/iti/iti.py
@@ -56,11 +56,12 @@ class iti_warehouse(orm.Model):
     _name = "iti.warehouse"
 
     _columns = {
-        'name':fields.char('name'),
-        'address': fields.char("Address"),
-       'keeper_id': fields.many2one('res.users', "Keeper"),
-        'manager_id': fields.many2one('res.users', "Manager"),
-        'super_manager_id': fields.many2one('res.users', "Super Manager", domain="[('id','=','ref('ourwarehouse.group_iti_warehouse_supermanager')')]"),
+        'name':fields.char('الاسم'),
+        'address': fields.char("العنوان"),
+		'date': fields.date('تاريخ الانشاء'),
+       'keeper_id': fields.many2one('res.users', "امين المخزن"),
+        'manager_id': fields.many2one('res.users', "مديرالمخزن"),
+        'super_manager_id': fields.many2one('res.users', "رئيس المخازن", domain="[('id','=','ref('ourwarehouse.group_iti_warehouse_supermanager')')]"),
 
     }
 
@@ -73,7 +74,7 @@ class iti_warehouse(orm.Model):
  #       'discription': fields.char("discription"),
  #     	'code':fields.integer("code"),
         # 'sub_category':fields.one2many('iti.subcategory', string="sub_category"),
-    }
+  #  }
 
 
 
